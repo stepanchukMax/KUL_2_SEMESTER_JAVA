9842035f7ec81a27e0527068abc7cbdd0dca12f1
stepanchukMax <maksymstepanchuk47@gmail.com>
2023-05-01 14:27:55 +0200

    Q2

--- test1/Q1/untitled/src/Main.java
+++ test1/Q1/untitled/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args){
-        Player player1 = new Player("Max",82);
+        Player player1 = new Player("Max",105);
         Player player2 = new Player("Yan",75);
-        Player player3 = new Player("Mo",77);
+        Player player3 = new Player("Mo",200);
         System.out.println("***************************");
         System.out.println("Is Max heavier than Yan?");
         System.out.println(player1.isHeavier(player2));
@@ -14,5 +14,22 @@ public class Main {
         player3.displayInformation();
         player2.displayInformation();
         player1.displayInformation();
+        Player[] listOfPlayers = new Player[3];
+        listOfPlayers[0] = player1;
+        listOfPlayers[1] = player2;
+        listOfPlayers[2] = player3;
+
+        System.out.println(howManyHundredKilos(listOfPlayers));
+
+
+    }
+    public static int howManyHundredKilos(Player[] playersList){
+        int counter=0;
+        for (Player player : playersList){
+            if (player.getWeight()>100){
+                counter++;
+            }
+        }
+        return counter;
     }
 }
